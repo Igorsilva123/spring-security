6ef51da2e353cbef7dd7c981088b552eab7f6fb0
Igorsilva123 <igor.silvabrito@outlook.com>
2025-10-07 17:13:19 -0300

    Alteracao usuario

--- src/main/java/spring/project/security/entity/user/User.java
+++ src/main/java/spring/project/security/entity/user/User.java
@@ -29,6 +29,8 @@ public class User implements UserDetails {
 
     private String cpf;
 
+    private String verifyToken;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
